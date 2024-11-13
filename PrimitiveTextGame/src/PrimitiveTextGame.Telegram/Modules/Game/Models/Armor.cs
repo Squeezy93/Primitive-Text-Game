f06c65339d363f46ccba3aa7dabba340efa72834
Pavel K. <el_passa@yahoo.com>
2024-11-13 18:05:36 +0100

    Telegram client for text game

---
+++ PrimitiveTextGame/src/PrimitiveTextGame.Telegram/Modules/Game/Models/Armor.cs
@@ -0,0 +1,6 @@
+namespace PrimitiveTextGame.Telegram.Modules.Game.Models;
+
+public class Armor
+{
+	public Guid Id { get; set; }
+}
---
+++ PrimitiveTextGame/src/PrimitiveTextGame.Telegram/Modules/Game/Models/Character.cs
@@ -0,0 +1,6 @@
+namespace PrimitiveTextGame.Telegram.Modules.Game.Models;
+
+public class Character
+{
+	public Guid Id { get; set; }
+}
